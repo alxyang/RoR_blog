ed4ff4b2c3358742608cb5671da154e5b8161905
lib-ayang <lib-ayang@pc12674.ucsd.edu>
2013-10-28 14:33:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppTwo::Application.config.secret_token = '33d8c24bf0c5415260606a5519e2d338cbc3a8e76ec69e0244540e4a83d75ca4eef2e51fc2bac092ed2dd7214d55030260e278f02f6efe6debd8b32ea1639174'
